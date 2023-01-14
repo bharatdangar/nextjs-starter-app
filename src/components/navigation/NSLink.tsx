d1928d5ea3cf5daa89467cea4e9052bee9bf2e94
Andreas Antonsson <andreasantonsson94@gmail.com>
2023-01-14 15:00:20 +0100

    feat: add className prop to NSLink

--- src/components/navigation/NSLink.tsx
+++ src/components/navigation/NSLink.tsx
@@ -3,10 +3,11 @@ import Link, { LinkProps } from 'next/link'
 
 interface Props extends LinkProps {
     children: ReactNode
+    className?: string
 }
 
-const NSLink: FC<Props> = ({ children, href, passHref }) => (
-  <Link href={href} passHref={passHref} scroll={false}>
+const NSLink: FC<Props> = ({ children, href, passHref, className }) => (
+  <Link href={href} passHref={passHref} scroll={false} className={className}>
     {children}
   </Link>
 )
